613935ce90f8b0d82769b07975da03ba33b66a10
riyansasaja <riyanboyuhu@gmail.com>
2021-06-07 11:04:09 +0800

    Selesai Output Berkas --  Menuju Tampilan User Profile

---
+++ application/views/banding/userprofile.php
@@ -0,0 +1,27 @@
+<div class="container-fluid px4">
+
+    <div class="row">
+        <div class="col">
+            <h1 class="mt-5 text-dasar">DASHBOARD EDIT PROFILE</h1>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="text-center">
+                <img src="<?= base_url('assets/') ?>img/user_logo.png" alt="">
+                <p>Pengadilan Agama Amurang</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    This is some text within a card body.
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
