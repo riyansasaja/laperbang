e8f36ed00628f132978251641eb483c7eaf22990
riyansasaja <riyanboyuhu@gmail.com>
2021-06-22 08:12:06 +0800

    merubah var ke let

--- assets/js/uploadbanding.js
+++ assets/js/uploadbanding.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $('#modalPdf').on('show.bs.modal', function (e) {
-        var getdata = $(e.relatedTarget).data('id');
+        let getdata = $(e.relatedTarget).data('id');
         console.log(getdata);
-        var baru = getdata.trim();
-        var tampil = `<embed src="http://localhost/laperbang/assets/files/SuratPengantar/${baru}" type="application/pdf" width="100%" height="100%">`
+        let baru = getdata.trim();
+        let tampil = `<embed src="http://localhost/laperbang/assets/files/SuratPengantar/${baru}" type="application/pdf" width="100%" height="100%">`
         $('#tampil').html(tampil);
     })
 });
