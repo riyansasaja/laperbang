05bd47a2e357af1607287a72c5536508c7c1d1f9
rizafdian <afdianriz@gmail.com>
2021-06-23 08:49:31 +0700

    menambah database baru

--- vendors/manado_laperbang.sql
+++ vendors/manado_laperbang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2021 at 04:46 AM
+-- Generation Time: Jun 23, 2021 at 03:48 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.1.32
 
@@ -107,12 +107,14 @@ CREATE TABLE `list_perkara` (
   `id_perkara` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `no_perkara` varchar(50) NOT NULL,
-  `nm_pihak` varchar(100) DEFAULT NULL,
+  `nm_pihak_penggugat` varchar(100) DEFAULT NULL,
+  `nm_pihak_tergugat` varchar(100) NOT NULL,
   `jns_perkara` varchar(50) NOT NULL,
   `tgl_register` date NOT NULL,
   `no_surat_pengantar` varchar(250) NOT NULL,
-  `nm_panitera` varchar(250) NOT NULL,
-  `nip_panitera` varchar(250) NOT NULL,
+  `pejabat_berwenang` varchar(100) NOT NULL,
+  `nm_pejabat` varchar(250) NOT NULL,
+  `nip_pejabat` varchar(250) NOT NULL,
   `banyaknya` int(11) NOT NULL,
   `keterangan` text NOT NULL,
   `status_perkara` varchar(50) DEFAULT NULL,
@@ -158,13 +160,17 @@ CREATE TABLE `list_perkara` (
 -- Dumping data for table `list_perkara`
 --
 
-INSERT INTO `list_perkara` (`id_perkara`, `id_user`, `no_perkara`, `nm_pihak`, `jns_perkara`, `tgl_register`, `no_surat_pengantar`, `nm_panitera`, `nip_panitera`, `banyaknya`, `keterangan`, `status_perkara`, `sp_perkara`, `no_perkara_banding`, `putusan_banding`, `is_nomor`, `surat_gugatan`, `sk_bundelA`, `bukti_pemb_panjar`, `majelis_hakim`, `penunjukan_pp`, `penunjukan_js`, `penetapan_hari_sidang`, `relaas_panggilan`, `ba_sidang`, `penetapan_sita`, `ba_sita`, `lampiran_surat`, `surat_bukti_penggugat`, `surat_bukti_tergugat`, `tanggapan_bukti_tergugat`, `tanggapan_bukti_penggugat`, `gambar_situasi`, `surat_lain`, `salinan_putusan_pa`, `sk_bundelb`, `akta_banding`, `akta_penerimaan_mb`, `memori_banding`, `akta_pemberitahuan_banding`, `pemberitahuan_penyerahan_mb`, `akta_penerimaankontra_mb`, `kontra_mb`, `pemberitahuan_penyerahankontra_mb`, `relaas_periksa_berkas_pb`, `sk_khusus`, `bukt_pengiriman_bpb`, `bukti_setor_bp_kasnegara`) VALUES
-(26, 2, 'XI/15/06', 'dani', 'poligami', '2021-06-15', 'XI/2021', 'rani', '1239999', 1, 'tes', '', '', '', '', 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(27, 2, 'XI/13/06/2021', 'zana', 'poligami', '2021-06-15', '12345', 'raka hj', '12346788889', 12, 'tesi', '', 'e-hac11.pdf', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(28, 6, '1111', 'dade', 'cerai', '2021-06-15', '2222', 'wer', '123', 2, 'testes', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(29, 6, '123', 'nana', 'poligami', '0000-00-00', '345', 'dadat', '1234567', 2, 'tes', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(30, 3, '12334355466', 'tyyyy', 'cerai', '0000-00-00', '344444', 'tyyyy', '455555', 2, 'tessss', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(31, 3, '5677777', 'rika', 'poligami', '2021-06-15', '4565677', 'yani', '343464557', 3, 'tes', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `list_perkara` (`id_perkara`, `id_user`, `no_perkara`, `nm_pihak_penggugat`, `nm_pihak_tergugat`, `jns_perkara`, `tgl_register`, `no_surat_pengantar`, `pejabat_berwenang`, `nm_pejabat`, `nip_pejabat`, `banyaknya`, `keterangan`, `status_perkara`, `sp_perkara`, `no_perkara_banding`, `putusan_banding`, `is_nomor`, `surat_gugatan`, `sk_bundelA`, `bukti_pemb_panjar`, `majelis_hakim`, `penunjukan_pp`, `penunjukan_js`, `penetapan_hari_sidang`, `relaas_panggilan`, `ba_sidang`, `penetapan_sita`, `ba_sita`, `lampiran_surat`, `surat_bukti_penggugat`, `surat_bukti_tergugat`, `tanggapan_bukti_tergugat`, `tanggapan_bukti_penggugat`, `gambar_situasi`, `surat_lain`, `salinan_putusan_pa`, `sk_bundelb`, `akta_banding`, `akta_penerimaan_mb`, `memori_banding`, `akta_pemberitahuan_banding`, `pemberitahuan_penyerahan_mb`, `akta_penerimaankontra_mb`, `kontra_mb`, `pemberitahuan_penyerahankontra_mb`, `relaas_periksa_berkas_pb`, `sk_khusus`, `bukt_pengiriman_bpb`, `bukti_setor_bp_kasnegara`) VALUES
+(26, 2, 'XI/15/06', 'dani', '', 'poligami', '2021-06-15', 'XI/2021', '', 'rani', '1239999', 1, 'tes', '', '', '', '', 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(27, 2, 'XI/13/06/2021', 'zana', '', 'poligami', '2021-06-15', '12345', '', 'raka hj', '12346788889', 12, 'tesi', '', 'e-hac11.pdf', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(28, 6, '1111', 'dade', '', 'cerai', '2021-06-15', '2222', '', 'wer', '123', 2, 'testes', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(29, 6, '123', 'nana', '', 'poligami', '0000-00-00', '345', '', 'dadat', '1234567', 2, 'tes', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(30, 3, '12334355466', 'tyyyy', '', 'cerai', '0000-00-00', '344444', '', 'tyyyy', '455555', 2, 'tessss', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(31, 3, '5677777', NULL, '', 'poligami', '2021-06-15', '4565677', '', '', '', 3, 'tes', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(34, 8, '213123', 'fsdfsdf', '', 'poligami', '2021-06-18', '3435345', '', 'dfdfgdf', '34345345', 2, '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(35, 8, '234234', 'ererte', '', 'cerai', '2021-06-18', '45345', '', 'scsdf', '455', 3, '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(36, 10, 'XI/13/06/2021', 'dfsfsdfg', '', 'poligami', '2021-06-18', '4535435', '', 'dfdgd', 'dfgdg', 1, 'dsfdfg', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(37, 2, '23234234', 'wwrewer', 'sdfsfs', 'cerai', '2021-06-23', '4324243', 'panitera', 'fsfds', '23423434554654657', 1, 'sfefre', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -175,6 +181,7 @@ INSERT INTO `list_perkara` (`id_perkara`, `id_user`, `no_perkara`, `nm_pihak`, `
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
@@ -186,18 +193,18 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `nama`, `username`, `password`, `role_id`, `is_active`, `data_created`) VALUES
-(1, 'Pengadilan Tinggi Agama Manado', 'pta-manado', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 1, 1, '2021-05-27'),
-(2, 'Pengadilan Agama Manado', 'pa-manado', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(3, 'Pengadilan Agama Tutuyan', 'pa-tutuyan', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(4, 'Pengadilan Agama Bolaang Uki', 'pa-blu', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(5, 'Pengadilan Agama Tondano', 'pa-tondano', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(6, 'Pengadilan Agama Lolak', 'pa-lolak', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(7, 'Pengadilan Agama Boroko', 'pa-boroko', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(8, 'Pengadilan Agama Amurang', 'pa-amurang', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(9, 'Pengadilan Agama Kotamobagu', 'pa-kotamobagu', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(10, 'Pengadilan Agama Tahuna', 'pa-tahuna', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
-(11, 'Pengadilan Agama Bitung', 'pa-bitung', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27');
+INSERT INTO `users` (`id`, `nama`, `email`, `username`, `password`, `role_id`, `is_active`, `data_created`) VALUES
+(1, 'Pengadilan Tinggi Agama Manado', '', 'pta-manado', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 1, 1, '2021-05-27'),
+(2, 'Pengadilan Agama Manado', '', 'pa-manado', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(3, 'Pengadilan Agama Tutuyan', '', 'pa-tutuyan', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(4, 'Pengadilan Agama Bolaang Uki', '', 'pa-blu', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(5, 'Pengadilan Agama Tondano', '', 'pa-tondano', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(6, 'Pengadilan Agama Lolak', '', 'pa-lolak', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(7, 'Pengadilan Agama Boroko', '', 'pa-boroko', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(8, 'Pengadilan Agama Amurang', '', 'pa-amurang', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(9, 'Pengadilan Agama Kotamobagu', '', 'pa-kotamobagu', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(10, 'Pengadilan Agama Tahuna', '', 'pa-tahuna', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27'),
+(11, 'Pengadilan Agama Bitung', '', 'pa-bitung', '$2y$10$zKNxC63heZCgpGadi9mWVO9.zBp5FcUE/mjdJjMrDBj/z/6IctVue', 2, 1, '2021-05-27');
 
 --
 -- Indexes for dumped tables
@@ -262,7 +269,7 @@ ALTER TABLE `kategori_perkara`
 -- AUTO_INCREMENT for table `list_perkara`
 --
 ALTER TABLE `list_perkara`
-  MODIFY `id_perkara` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_perkara` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `users`
