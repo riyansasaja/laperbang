daafe005d05235fed64e5bc056592ce8fa8d12c7
riyansasaja <riyanboyuhu@gmail.com>
2021-06-24 11:50:43 +0800

    membuat tampilan hakim/banding

---
+++ application/views/hakim/banding.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <div class="row mt-5">
+        <div class="col">
+            <table class="table table-responsive" id="tablePerkara">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nomor Perkara</th>
+                        <th>Jenis Perkara</th>
+                        <th>Nomor Perkara Banding</th>
+                        <th>Tanggal Register</th>
+                        <th>Status</th>
+                        <th style="width: 10%;">Lihat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>1</th>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
