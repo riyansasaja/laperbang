1083a215c07d558a8bd3857da9166c6b6677c115
riyansasaja <riyanboyuhu@gmail.com>
2021-06-25 12:45:30 +0800

    tampilan admin dashboard telah tampil

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,4 +3,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Admin extends CI_Controller
 {
+
+    public function index()
+    {
+
+        //konten
+        $data['judul'] = 'Dashboard';
+        $data['css'] = 'dashboard_admin.css';
+        $data['js'] = 'dashboard_admin.js';
+
+        $this->load->view('admin/header', $data);
+        $this->load->view('admin/dashboard', $data);
+        $this->load->view('admin/footer', $data);
+    }
 }
