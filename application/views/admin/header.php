f7c301aede2fa52f665b404fd5d90ed5c5f1f70d
riyansasaja <riyanboyuhu@gmail.com>
2021-06-29 18:23:25 +0800

    perubahan di header

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -41,7 +41,7 @@
             <!-- Navbar-->
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle animate__animated animate__heartBeat animate__infinite" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fas fa-envelope-open fa-fw"></i>
                         <span class="position-absolute top-6 translate-middle badge rounded-pill bg-danger animate__animated animate__flash animate__infinite">
                             2
@@ -57,7 +57,7 @@
             </ul>
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <a class="nav-link dropdown-toggle animate__animated animate__heartBeat" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="#!">Settings</a></li>
                         <li><a class="dropdown-item" href="#!">Activity Log</a></li>
