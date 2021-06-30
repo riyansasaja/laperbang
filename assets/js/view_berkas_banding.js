75bcdfe2c31117d7c4b01863622297ba8cf1800d
riyansasaja <riyanboyuhu@gmail.com>
2021-06-30 08:43:23 +0800

    selesai fungsi tampil

--- assets/js/view_berkas_banding.js
+++ assets/js/view_berkas_banding.js
@@ -6,37 +6,16 @@ $(document).ready(function () {
     let pecah = url.split('/');
     let id_perkara = pecah[6];
     $('#modalPdfHakim').on('show.bs.modal', function (e) {
+
+        //jalankan modal
+        //ambil data-id dan data-judul
         let getdata = $(e.relatedTarget).data('id');
         let getjudul = $(e.relatedTarget).data('judul');
-        console.log(getdata);
-        let tampil = `<embed src="http://localhost/laperbang/assets/files/${getdata}" type="application/pdf" width="100%" height="100%">`
-
+        //embed ke tampilan sebelah kanan
+        let tampil = `<embed src="http://localhost/laperbang/assets/files/${getdata}" type="application/pdf" width="100%" height="100%">`;
         $('#tampil').html(tampil);
-        $('#komentar').html('');
-        tampil_baru();
 
-
-        $('#kirim').on('click', function () {
-            var catatan = $('textarea[name="catatan"]').val();
-
-
-            $.ajax({
-                type: "POST",
-                url: `${path}ViewHakim/set_catatan`,
-                data: {
-                    id_perkara: id_perkara,
-                    nm_berkas: getjudul,
-                    catatan: catatan
-
-                },
-                dataType: "json",
-                success: function (response) {
-                    tampil_baru();
-
-                }
-            });
-        });
-
+        //fungsi tampil baru
         function tampil_baru() {
             $.ajax({
                 type: "POST",
@@ -47,27 +26,54 @@ $(document).ready(function () {
                 },
                 dataType: "json",
                 success: function (e) {
-
-                    $.each(e, function (i, v) {
-                        console.log(v);
-                        let isi = `<p><span class="fw-bolder ps-3">${v.nama}</span> <small class="text-muted">${v.time}</small> <br><small class="ps-3">${v.catatan}</small></p>`
-
-                        $('#komentar').append(isi);
-
-
-
+                    let isi = '';
+                    $.each(e, function (index, value) {
+                        isi += `<p><span class="fw-bolder ps-3">${value.nama}</span> <small class="text-muted">${value.time}</small> <br><small class="ps-3">${value.catatan}</small></p>`;
+                        return
                     });
-
-
+                    console.log(isi);
+                    $('#komentar').html(isi);
                 }
-            });
+            });//tutup ajax
             $('#komentar').html('');
             $('#catatan').val('');
+            return;
+
+        }//end fungsi tampil baru
+
+        //kosongkan komentar
+        $('#komentar').html('');
+        // panggil fungsi tampil baru
+        tampil_baru();
 
 
 
-        }
-    })
+
+        //tombol kirim ditekan
+        $('#kirim').on('click', function () {
+            var catatan = $('textarea[name="catatan"]').val();
+            $.ajax({
+                type: "POST",
+                url: `${path}ViewHakim/set_catatan`,
+                data: {
+                    id_perkara: id_perkara,
+                    nm_berkas: getjudul,
+                    catatan: catatan
+                },
+                dataType: "json",
+                success: function (response) {
+                    tampil_baru();
+
+                }
+            });//end ajax
+        }); //end tombol kirim ditekan
 
 
-});
+
+
+    })//end modal
+
+
+
+
+});//end doc ready
