e2b2ca8a7cad48127e0c9cceb387beeac83ba0e5
riyansasaja <riyanboyuhu@gmail.com>
2021-08-05 10:58:45 +0800

    tambah jquery di banding footer

--- application/views/banding/footer.php
+++ application/views/banding/footer.php
@@ -25,7 +25,7 @@
 <!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 <!-- javascript -->
-<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="<?= base_url('assets/') ?>jquery/jquery.min.js"></script>
 <!-- datatables -->
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
 <!-- sweet alert -->
