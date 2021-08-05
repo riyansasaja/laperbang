9bdf0118464ffecb36be0e0baf8b6ecade031a06
riyansasaja <riyanboyuhu@gmail.com>
2021-08-05 18:48:35 +0800

    0805 riyan localhost tambah logout di home

--- application/views/dashboard/home.php
+++ application/views/dashboard/home.php
@@ -1,7 +1,32 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-12 bg-dua border-satu">
-            <p class="text-satu text-center mt-3">LAYANAN ADMINISTRASI PERKARA BANDING <br>PENGADILAN TINGGI AGAMA MANADO</p>
+        <div class="col-10 bg-dua border-satu">
+            <p class="text-satu text-center mt-3" style="margin-left: 30%;">LAYANAN ADMINISTRASI PERKARA BANDING <br>PENGADILAN TINGGI AGAMA MANADO</p>
+        </div>
+        <div class="col-2 bg-dua border-satu">
+
+            <nav class="navbar navbar-expand-lg navbar-dark">
+
+                <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                User
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="<?= base_url('banding/userProfile'); ?>">Profile</a>
+                                <a class="dropdown-item" href="<?= base_url('auth/logout'); ?>">Logout</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
+            <!-- <p class="text-satu text-center mt-3">LAYANAN ADMINISTRASI PERKARA BANDING <br>PENGADILAN TINGGI AGAMA MANADO</p> -->
         </div>
     </div>
     <div class="row">
