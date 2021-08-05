ad5e7a492e1c512716a069193066f5839fffa9ca
riyansasaja <riyanboyuhu@gmail.com>
2021-08-05 18:24:32 +0800

    0805 riyan -- bekeng ulang depe navbar

--- application/views/dashboard/home.php
+++ application/views/dashboard/home.php
@@ -1,24 +1,6 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-12 bg-dua border-satu">
-
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            <a class="nav-link" href="#">Features</a>
-                            <a class="nav-link" href="#">Pricing</a>
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
             <!-- <p class="text-satu text-center mt-3">LAYANAN ADMINISTRASI PERKARA BANDING <br>PENGADILAN TINGGI AGAMA MANADO</p> -->
         </div>
     </div>
