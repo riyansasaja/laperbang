5da627e2baf02f0f54a44bed032c3eb855aa4b4d
riyansasaja <riyanboyuhu@gmail.com>
2021-08-05 19:59:12 +0800

    0805 riyan -- coba fix5 admin view berkas admin

--- assets/js/view_berkas_admin.js
+++ assets/js/view_berkas_admin.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
+    const path = window.location.origin;
     $('#modalPdfAdmin').on('show.bs.modal', function (e) {
         let getdata = $(e.relatedTarget).data('id');
         console.log(getdata);
-        let tampil = `<embed src="http://laperbang.pta-manado.go.id/assets/files/${getdata}" type="application/pdf" width="100%" height="100%">`
+        let tampil = `<embed src="${path}/assets/files/${getdata}" type="application/pdf" width="100%" height="100%">`
         $('#tampil').html(tampil);
-        console.log(getdata);
     })
 });
