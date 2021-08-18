7180e0c08b0ba46b46f8d273a8963bc369e24494
riyansasaja <riyanboyuhu@gmail.com>
2021-08-18 14:05:32 +0800

    try fix mail 19

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -132,7 +132,7 @@ class Auth extends CI_Controller
     {
         $config = [
             'protocol'  => 'ssmtp',
-            'smtp_host' => 'ssl://ssmtp.googlemail.com',
+            'smtp_host' => 'ssl://ssmtp.gmail.com',
             'smtp_user' => 'laperbang.ptamanado@gmail.com',
             'smtp_pass' => 'laperbang1234',
             'smtp_port' => 465,
