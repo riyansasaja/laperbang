24407d6df2e633407375ad7cace35cae9ba32979
riyansasaja <riyanboyuhu@gmail.com>
2021-08-24 16:03:47 +0800

    fix qr - 1

--- application/views/showid/detil_qrdok.php
+++ application/views/showid/detil_qrdok.php
@@ -1,73 +1,71 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <!-- Meta, title, CSS, favicons, etc. -->
+  <!-- Required meta tags -->
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="images/favicon.ico" type="image/ico" />
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+  <!-- css sandiri -->
+  <link rel="stylesheet" href="<?= base_url('assets/css/') ?>cssribon.css">
 
   <title>LAPERBANG | Pengadilan Tinggi Agama Manado</title>
-
-  <!-- Bootstrap -->
-  <link href="<?php echo base_url(); ?>assets/css/showdoc/bootstrap.css" rel="stylesheet">
-
-  <!-- Custom Theme Style -->
-  <link href="<?php echo base_url(); ?>assets/css/showdoc/custom.css" rel="stylesheet">
 </head>
 
-<body>
-  <div class="container body">
-    <div class="main_container">
-      <div class="x_panel ui-ribbon-container ui-ribbon-primary">
-        <div class="ui-ribbon-wrapper">
-          <div class="ui-ribbon">
-            Sah
-          </div>
-        </div>
-        <div class="x_title">
-          <h3 class="name_title">Deskripsi Dokumen</h3>
-          <div class="clearfix"></div>
-        </div>
-        <div class="x_content">
+<body class="bg-white">
 
-          <div style="text-align: center; margin-bottom: 17px">
-            <span class="chart" data-percent="86">
-              <span>
-                <img width="80px" src="<?php echo base_url('qrcodeimg/qr_' . $dok_id . '.png') ?>">
-              </span>
-            </span>
-          </div>
-          <div class="divider"></div>
-          <p>Ini adalah detil dokumen dari pengajuan perkara banding yang benar diterbikan oleh aplikasi LAPERBANG Pengadilan Tinggi Agama Manado.</p>
+  <div class="container">
 
-          <div class="divider"></div>
-          <h5>Satker</h5>
-          <p><?php echo $nama; ?></p>
-          <div class="divider"></div>
-          <h5>Nomor Perkara</h5>
-          <p><?php echo $no_perkara; ?></p>
-          <div class="divider"></div>
-          <h5>Nomor Surat Pengantar</h5>
-          <p><?php echo $no_surat_pengantar; ?></p>
-          <div class="divider"></div>
-          <h5>Pejabat Berwenang</h5>
-          <p><?php echo $pejabat_berwenang; ?></p>
-          <div class="divider"></div>
-          <h5>Nama</h5>
-          <p><?php echo $nm_pejabat; ?></p>
-          <div class="divider"></div>
-          <h5>NIP</h5>
-          <p><?php echo $nip_pejabat; ?></p>
-          <!-- <div class="divider"></div> 
-              <h5>Tanggal Register</h5>
-              <p><?php echo indonesian_date($tglinput); ?></p> -->
-          <div class="divider"></div>
-          <br><br><br><br><br><br>
-        </div>
+    <div class="ribbon ribbon-top-right"><span>Sah</span></div>
+
+
+    <div class="row">
+      <div class="col">
+        <h3 class="name_title">Deskripsi Dokumen</h3>
+        <span>
+          <img width="80px" src="<?php echo base_url('qrcodeimg/qr_' . $dok_id . '.png') ?>">
+        </span>
+        <p>Ini adalah detil dokumen dari pengajuan perkara banding yang benar diterbikan oleh aplikasi LAPERBANG Pengadilan Tinggi Agama Manado.</p>
+        <hr>
+        <h5>Satker</h5>
+        <p><?php echo $nama; ?></p>
+        <hr>
+        <h5>Nomor Perkara</h5>
+        <p><?php echo $no_perkara; ?></p>
+        <hr>
+        <h5>Nomor Surat Pengantar</h5>
+        <p><?php echo $no_surat_pengantar; ?></p>
+        <hr>
+        <h5>Pejabat Berwenang</h5>
+        <p><?php echo $pejabat_berwenang; ?></p>
+        <hr>
+        <h5>Nama</h5>
+        <p><?php echo $nm_pejabat; ?></p>
+        <hr>
+        <h5>NIP</h5>
+        <p><?php echo $nip_pejabat; ?></p>
+        <hr>
       </div>
     </div>
 
+
+  </div>
+
+
+
+
+
+
+
+
+  <!-- =========================== -->
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
 </body>
+
+</html>
