fb43ab61f720b0886baae37fe4e57417d8af6e0e
rizafdian <afdianriz@gmail.com>
2021-08-27 12:53:26 +0700

    update

--- application/views/panmud/view_berkas_admin.php
+++ application/views/panmud/view_berkas_admin.php
@@ -8,7 +8,7 @@
 
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingOne">
-                            <button class="accordion-button collapse" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                 Surat Pengantar
                             </button>
                         </h2>
@@ -34,7 +34,7 @@
 
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingTwo">
-                            <button class="accordion-button collapse" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
                                 Bundel A
                             </button>
                         </h2>
