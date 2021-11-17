c1f751531f8f26a39d3725fcefd0585ba7471984
rizafdian <afdianriz@gmail.com>
2021-11-17 15:15:46 +0700

    171121

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://localhost/laperbang/';
+$config['base_url'] = 'https://ujicoba.pta-manado.go.id/laperbang/';
 
 /*
 |--------------------------------------------------------------------------
